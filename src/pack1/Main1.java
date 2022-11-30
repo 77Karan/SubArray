5f01d368f00bd4c1ed4926e8c5924112407694e2
karankumar <kkarankumar890@gmail.com>
2022-11-30 15:11:40 +0530

    first commit

---
+++ src/pack1/Main1.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Main1 
+{
+	public static void main(String[] args)
+	{
+		int[] arr = {12,12,2,1};
+		Integer size = arr.length;
+		System.out.println(Main.maxSubarrayProduct(arr, size));
+		
+	}
+
+}
